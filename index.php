07bfda013d79b2a47644097823aba5a3bcffd863
DIGBRUCE7 <Kewell77>
2016-10-03 15:28:23 +0100

    fed

--- index.php
+++ index.php
@@ -5,36 +5,11 @@
 <body>
 <p>
     <?php
-    $age = 19;
-    $wantedGoods = "mugs";
-    /*
-    if ($age < 16) {
-        print "You can't buy anything.";
+    $provisionedActivities = array("Specs", "Mugs", "Sausage Rolls"); // declares the array
+    foreach($provisionedActivities as $x) {
+        print "<p>$x</p>";
     }
-    elseif ($age >= 21) {
-        print "You can buy specs, mugs and sausage rolls!";
-    }
-    elseif ($age < 18) {
-        print "You can buy specs.";
-    }
-    else {
-        print "You can buy specs and mugs";
-    }
-    */
 
-    switch($wantedGoods) {
-        case "specs":
-            echo "You need to be 16 to buy specs.";
-            break;
-        case "mugs":
-            echo "You need to be 18 to buy mugs.";
-            break;
-        case "sausage rolls":
-            echo "You need to be 21 to buy sausage rolls.";
-            break;
-        default:
-            echo "That isn't an option!!!!";
-    }
     ?>
 </p>
 </body>
