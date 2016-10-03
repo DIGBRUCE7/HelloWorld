098afa1f9d22d2590201658f62a56daf4e633c95
DIGBRUCE7 <Kewell77>
2016-10-03 15:22:53 +0100

    fed

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <p>
     <?php
     $age = 19;
+    $wantedGoods = "mugs";
     if ($age < 16) {
         print "You can't buy anything.";
     }
@@ -17,6 +18,20 @@
     }
     else {
         print "You can buy specs and mugs";
+    }
+
+    switch($wantedGoods) {
+        case "specs":
+            echo "You need to be 16 to buy specs.";
+            break;
+        case "mugs":
+            echo "You need to be 18 to buy mugs.";
+            break;
+        case "sausage rolls":
+            echo "You need to be 21 to buy sausage rolls.";
+            break;
+        default:
+            echo "That isn't an option!!!!";
     }
     ?>
 </p>
