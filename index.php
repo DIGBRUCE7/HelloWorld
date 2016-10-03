27c99f4a04b1b41af94a681ed61a31e0bf670fce
DIGBRUCE7 <Kewell77>
2016-10-03 15:15:54 +0100

    fed

--- index.php
+++ index.php
@@ -5,7 +5,19 @@
 <body>
 <p>
     <?php
-    echo "Hello World";
+    $age = 19;
+    if ($age < 16) {
+        print "You can't buy anything.";
+    }
+    elseif ($age >= 21) {
+        print "You can buy specs, mugs and sausage rolls!";
+    }
+    elseif ($age < 18) {
+        print "You can buy specs.";
+    }
+    else {
+        print "You can buy specs and mugs";
+    }
     ?>
 </p>
 </body>
