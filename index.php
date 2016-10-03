91ee107216ce24aff64519f0cdf85d719436b2e4
DIGBRUCE7 <Kewell77>
2016-10-03 15:57:10 +0100

    fed

--- index.php
+++ index.php
@@ -10,24 +10,24 @@
     $rollsAvailable = 0;
     for ($i = 1; $i < 31; $i++)
     {
-        $mugsAvailable = $mugsAvailable % $i;
-        $specsAvailable = $specsAvailable % $i;
-        $rollsAvailable = $rollsAvailable % $i;
+        $mugsAvailable = $i % 3;
+        $specsAvailable = $i % 2;
+        $rollsAvailable = $i & 4;
 
         if ($mugsAvailable == 0 && $specsAvailable == 0 && $rollsAvailable == 0) {
-            print "On day " . $i . " of the month specs, mugs and sausage rolls are all available.";
+            print "On day " . $i . " of the month specs, mugs and sausage rolls are all available." . "\r\n";
         }
         elseif ($mugsAvailable == 0 && $specsAvailable == 0) {
-            print "On day " . $i . " of the month specs and mugs are available.";
+            print "On day " . $i . " of the month specs and mugs are available." . "\r\n";
         }
         elseif ($mugsAvailable == 0 && $rollsAvailable == 0) {
-            print "On day " . $i . " of the month mugs and rolls are available.";
+            print "On day " . $i . " of the month mugs and rolls are available." . "\r\n";
         }
         elseif ($specsAvailable == 0 && $rollsAvailable == 0) {
-                print "On day " . $i . " of the month mugs and rolls are available.";
+                print "On day " . $i . " of the month mugs and rolls are available." . "\r\n";
         }
         else {
-            print "On day " . $i . " of the month no products are available.";
+            print "On day " . $i . " of the month no products are available." . "\r\n";
         }
     }
     ?>
