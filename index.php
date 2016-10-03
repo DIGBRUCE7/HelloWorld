9bb5886bdfc61cb52afbdade3389e226adafa163
DIGBRUCE7 <Kewell77>
2016-10-03 15:31:56 +0100

    fed

--- index.php
+++ index.php
@@ -9,8 +9,16 @@
     foreach($provisionedActivities as $x) {
         print "<p>$x</p>";
     }
+
     $provisionedActivities[1] = "Hugs"; // modifies position 1 (Mugs)
-    echo $provisionedActivities[1] // outputs "Hugs"
+    foreach($provisionedActivities as $x) {
+        print "<p>$x</p>";
+    }
+
+    unset($provisionedActivities[2]); // removes the array in position 2 (Sausage Rolls
+    foreach($provisionedActivities as $x) {
+        print "<p>$x</p>";
+    }
     ?>
 </p>
 </body>
