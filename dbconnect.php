ed569b1653990179015f05d55a4fda89b136cfd6
DIGBRUCE7 <Kewell77>
2016-10-10 14:23:10 +0100

    fed

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,8 @@
 <?php
 
-define('DB_SERVER', '')
+define('DB_SERVER', 'eu-cdbr-azure-west-a.cloudapp.net');
+define('DB_USERNAME', 'bc35ba43396f27');
+define('DB_PASSWORD', '6fab8060');
+define('DB_DATABASE', 'db_7' );
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
--- index.php
+++ index.php
@@ -1,35 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>DB7</title>
 </head>
 <body>
+<h1> <a href="dbconnect.php"> DB PHP! </a>  </h1>
 <p>
     <?php
-    $mugsAvailable = 0;
-    $specsAvailable = 0;
-    $rollsAvailable = 0;
-    for ($i = 1; $i < 31; $i++)
-    {
-        $mugsAvailable = $i % 3;
-        $specsAvailable = $i % 2;
-        $rollsAvailable = $i & 4;
 
-        if ($mugsAvailable == 0 && $specsAvailable == 0 && $rollsAvailable == 0) {
-            print "On day " . $i . " of the month specs, mugs and sausage rolls are all available." . "\r\n";
-        }
-        elseif ($mugsAvailable == 0 && $specsAvailable == 0) {
-            print "On day " . $i . " of the month specs and mugs are available." . "\r\n";
-        }
-        elseif ($mugsAvailable == 0 && $rollsAvailable == 0) {
-            print "On day " . $i . " of the month mugs and rolls are available." . "\r\n";
-        }
-        elseif ($specsAvailable == 0 && $rollsAvailable == 0) {
-                print "On day " . $i . " of the month mugs and rolls are available." . "\r\n";
-        }
-        else {
-            print "On day " . $i . " of the month no products are available." . "\r\n";
-        }
-    }
     ?>
 </p>
 </body>
